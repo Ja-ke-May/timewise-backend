b620772d5fa2521b71499f1b19655c45be8ea6ff
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-03-17 07:09:29 +0000

    dateQuizTaken changed to string

--- models/leaderboard.js
+++ models/leaderboard.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const leaderboardSchema = mongoose.Schema({
 quizType: String,
 quizDate: String,
-dateQuizTaken: Date,
+dateQuizTaken: String,
   userName: String,
   totalScore: Number,
 
