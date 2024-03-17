b07c897fc51c132c3b9fed9553e8b2a976a68561
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-03-17 04:20:07 +0000

    dateQuizTaken

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ app.post('/leaderboard', async (req, res) => {
 
   app.get('/leaderboard', async (req, res) => {
     try {
-      const { quizType, quizDate } = req.query;
-      const leaderboardData = await Leaderboard.find({ quizType, quizDate });
+      const { quizType, quizDate, dateQuizTaken } = req.query;
+      const leaderboardData = await Leaderboard.find({ quizType, quizDate, dateQuizTaken });
       res.status(200).json(leaderboardData);
     } catch (error) {
       console.error('Error retrieving leaderboard data:', error);
