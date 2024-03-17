729a84d5dd95f07b7c572aa63a8cefb13be4d89d
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-03-17 23:06:40 +0000

    different leaderboards added to get request

--- index.js
+++ index.js
@@ -45,14 +45,24 @@ app.post('/leaderboard', async (req, res) => {
 app.get('/leaderboard', async (req, res) => {
   try {
     const { quizType, quizDate, dateQuizTaken } = req.query;
-    const questionsData = await Question.find({ quizType, quizDate, dateQuizTaken });
-    res.status(200).json(questionsData);
-    console.log(dateQuizTaken)
+
+    let leaderboardData;
+
+    if (dateQuizTaken) {
+      leaderboardData = await Leaderboard.find({ quizType, quizDate, dateQuizTaken });
+    } else if (quizDate) {
+      leaderboardData = await Leaderboard.find({ quizType, quizDate });
+    } else {
+      leaderboardData = await Leaderboard.find({ quizType });
+    }
+
+    res.status(200).json(leaderboardData);
   } catch (error) {
-    console.error('Error retrieving questions data:', error);
+    console.error('Error retrieving leaderboard data:', error);
     res.status(500).send('Internal Server Error');
   }
 });
+
 
 app.post('/questions', async (req, res) => {
     const { quizType, quizDate, question, options, correctAnswer } = req.body;
